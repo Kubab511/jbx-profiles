4972f670c4bf27fa7b039fcad56a1159b67b7b5d
Kubab511 <111919220+Kubab511@users.noreply.github.com>
2024-07-30 20:36:20 +0200

    Added contact section and discord link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function Contact() {
   return (
     <>
       <h1 className="pt-24 text-slate-950 dark:text-[#ADB7BE] text-center font-bold text-4xl">Contact</h1>
       <p className="text-slate-950 dark:text-[#ADB7BE] text-xl text-center px-2">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis commodi fugit quo, sunt blanditiis dolorem, excepturi aspernatur deleniti libero deserunt autem, iste mollitia illum ipsum? Possimus sapiente odio consequuntur molestiae!
+        If you have any questions, comments or issues reach out to us at <Link className="hover:text-neutral-600 dark:hover:text-white" to={"mailto:contact@jbx-profiles.org"}>contact@jbx-profiles.org</Link> or on our discord server linked below
       </p>
     </>
   )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,18 @@
+import { FaDiscord } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export function Footer() {
   return (
     <>
     <div className="footer border border-transparent border-t-[#484b6a] text-slate-950">
       <div className="container mx-auto p-6 flex flex-col items-center justify-between min-h-fit sm:flex-row">
         <img className="h-10 w-10" src="/vite.svg" alt="Icon" />
-        <p className="text-slate-600 md:pt-0 pt-2">Copyright &#169; JBX Profiles 2024</p>
+        <div className="flex">
+          <Link to={"https://discord.com/app"} target="_blank">
+            <FaDiscord className="h-10 w-10 sm:pt-0 pt-2 text-slate-950 hover:text-neutral-600 dark:text-[#ADB7BE] dark:hover:text-white" />
+          </Link>
+          <p className="text-slate-600 sm:pt-0 pt-2 my-auto pl-4">Copyright &#169; JBX Profiles 2024</p>
+        </div>
       </div>
     </div>
     </>
