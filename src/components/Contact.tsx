5adcf409c0373ae85bda88e5fcb5fed991da7645
Kubab511 <111919220+Kubab511@users.noreply.github.com>
2024-08-01 14:31:47 +0200

    Removed email address

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ export function Contact() {
     <>
       <h1 className="pt-24 text-slate-950 dark:text-[#ADB7BE] text-center font-bold text-4xl">Contact</h1>
       <p className="text-slate-950 dark:text-[#ADB7BE] text-xl text-center px-2">
-        If you have any questions, comments or issues reach out to us at <Link className="hover:text-neutral-600 dark:hover:text-white" to={"mailto:contact@jbx-profiles.org"}>contact@jbx-profiles.org</Link> or on our discord server linked below
+        If you have any questions, comments or issues reach out to us at <Link className="hover:text-neutral-600 dark:hover:text-white" to={"#"}>#</Link> or on our discord server linked below
       </p>
     </>
   )
