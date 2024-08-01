6a30d1085e38a445924137eb6635146288051f57
Kubab511 <111919220+Kubab511@users.noreply.github.com>
2024-08-01 14:24:47 +0200

    Removed discord invite

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
       <div className="container mx-auto p-6 flex flex-col items-center justify-between min-h-fit sm:flex-row">
         <img className="h-10 w-10" src="/vite.svg" alt="Icon" />
         <div className="flex">
-          <Link to={"https://discord.gg/HgZKAFQmtc"} target="_blank">
+          <Link to={"https://discord.com/app"} target="_blank">
             <FaDiscord className="h-10 w-10 sm:pt-0 pt-2 text-slate-950 hover:text-neutral-600 dark:text-[#ADB7BE] dark:hover:text-white" />
           </Link>
           <p className="text-slate-600 sm:pt-0 pt-2 my-auto pl-4">Copyright &#169; JBX Profiles 2024</p>
